2e00c818c910c777d325cab18146459d4b84d3da
jyochsna <55848802+jyochsna@users.noreply.github.com>
2020-06-26 17:23:24 -0700

    Updates

---
+++ precache-manifest.5f7ae38a0d6d55a44957d5c0e02e7baa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5843dcf1a30d1961e44d0346af1c6e80",
+    "url": "/lasoon/index.html"
+  },
+  {
+    "revision": "4b9bf9272e4dc078f9e9",
+    "url": "/lasoon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/lasoon/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lasoon/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9bf9272e4dc078f9e9",
+    "url": "/lasoon/static/js/main.93e3829d.chunk.js"
+  },
+  {
+    "revision": "c419fe08972800870814",
+    "url": "/lasoon/static/js/runtime-main.18530a51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lasoon/static/media/logo.5d5d9eef.svg"
+  }
+]);
